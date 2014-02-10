206400a384c4aabfb316f62529441a15bb63885f
vladimir <bfg.tyurin@gmail.com>
2014-02-10 20:59:21 +0400

    Initial commit convert String array to int array

---
+++ src/main/java/FileSize.java
@@ -0,0 +1,11 @@
+import java.io.File;
+
+/**
+ * Created by vladimir on 2/10/14.
+ */
+public class FileSize {
+    public static void main(String[] args) {
+        File file = new File(args[0]);
+        System.out.println(file.length());
+    }
+}

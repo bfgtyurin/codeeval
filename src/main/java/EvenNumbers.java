32c98117995660da66aa2d103322ae2df9f324e2
bfgtyurin <bfg.tyurin@gmail.com>
2014-02-16 20:14:42 +0400

    Initial commit

---
+++ src/main/java/EvenNumbers.java
@@ -0,0 +1,25 @@
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+/**
+ * Created by Vladimir on 16.02.14.
+ */
+public class EvenNumbers {
+    public static void main(String[] args) {
+        File file = new File(args[0]);
+        try {
+            Scanner in = new Scanner(new FileReader(file));
+            while (in.hasNextInt()) {
+                if ((in.nextInt() % 2) != 0) {
+                    System.out.println(0);
+                } else {
+                    System.out.println(1);
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("File read error " + e.getMessage());
+        }
+    }
+}
